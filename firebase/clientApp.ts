c38592f511e3ee1eb2448c7ddfeec1619fe8dc3a
yusufginanjar <yusufginanjar16@gmail.com>
2022-09-18 18:55:12 +0700

    Firebase: init

---
+++ firebase/clientApp.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDT04MwsliZA6PQgltRPA9v4Hr8T4IucMY",
+  authDomain: "game-asik-f1a97.firebaseapp.com",
+  projectId: "game-asik-f1a97",
+  storageBucket: "game-asik-f1a97.appspot.com",
+  messagingSenderId: "706326430588",
+  appId: "1:706326430588:web:14a00623a4ec770f8b9af9",
+  measurementId: "G-53F4G4YLMK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
---
+++ pages/game.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../firebase/clientApp';
+
+
+export default function Game() {
+  return (
+    <div>
+      <h1>Game</h1>
+    </div>
+  );
+}
